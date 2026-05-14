f4ce34bd19cb1fe6aae59be87fa67298a351b71c
Radmir Molotov <radmirmolotov@gmail.com>
2026-05-15 01:37:32 +0500

    ИспÐравленный проьÐбел

--- src/main/java/ru/yandex/practicum/sleeptracker/SleepAnalyzer.java
+++ src/main/java/ru/yandex/practicum/sleeptracker/SleepAnalyzer.java
@@ -1,7 +1,9 @@
 package ru.yandex.practicum.sleeptracker;
+
 import java.util.List;
 import java.util.function.Function;
 
-public interface SleepAnalyzer extends Function<List<SleepingSession>, SleepAnalysisResult>{
+public interface SleepAnalyzer
+        extends Function<List<SleepingSession>, SleepAnalysisResult> {
 
 }
